4d860433267b7ce978ad03ee5688fc8b2a0502bc
joshxyzhimself <joshxyzhimself@gmail.com>
2022-10-09 23:22:36 +0800

    save

--- crestfall-auth/index.mjs
+++ crestfall-auth/index.mjs
@@ -1,8 +1,10 @@
 // @ts-check
 
 import assert from 'assert';
-import env from '../env.mjs';
+import readline from 'node:readline/promises';
+import fetch from 'node-fetch';
 import * as jwt from './hs256.mjs';
+import env from '../env.mjs';
 
 console.log({ env });
 
@@ -13,14 +15,27 @@ assert(env.get('PGRST_JWT_SECRET_IS_BASE64') === 'true');
 const secret = env.get('PGRST_JWT_SECRET');
 console.log({ secret });
 
-const header = { alg: 'HS256', typ: 'JWT' };
-console.log({ header });
+const rli = readline.createInterface({ input: process.stdin, output: process.stdout });
 
-const payload = { role: 'anon' };
-console.log({ payload });
-
-const token = jwt.create_token(header, payload, secret);
-console.log({ token });
-
-const verified = jwt.verify_token(token, secret);
-console.log({ verified });
+const loop = async () => {
+  const line = await rli.question('');
+  switch (line) {
+    case 'ct': {
+      const header = { alg: 'HS256', typ: 'JWT' };
+      console.log({ header });
+      const payload = { role: 'anon' };
+      console.log({ payload });
+      const token = jwt.create_token(header, payload, secret);
+      console.log({ token });
+      const verified = jwt.verify_token(token, secret);
+      console.log({ verified });
+      break;
+    }
+    default: {
+      console.log(`Unhandled: ${line}`);
+      break;
+    }
+  }
+  process.nextTick(loop);
+};
+process.nextTick(loop);
