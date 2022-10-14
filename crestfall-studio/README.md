fa212fa87bfda2be65024462f1a78bb7d7349206
joshxyzhimself <joshxyzhimself@gmail.com>
2022-10-14 08:59:34 +0800

    crestfall studio docker instance

---
+++ crestfall-studio/README.md
@@ -0,0 +1,18 @@
+## Crestfall Studio
+
+### Notes
+
+#### Usage
+
+```sh
+cd ./crestfall-studio/
+npm install
+npm run studio-dev
+```
+
+#### Hard Links
+
+```sh
+ln ./package.json ./crestfall-studio/package.json
+ln ./package-lock.json ./crestfall-studio/package-lock.json
+```
--- crestfall-studio/vite.config.js
+++ crestfall-studio/vite.config.js
@@ -1,14 +1,13 @@
 // @ts-check
 // https://vitejs.dev/config/
 
-import path from 'path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  root: path.join(process.cwd(), './crestfall-studio/client/'),
+  root: './client/',
   build: {
-    outDir: path.join(process.cwd(), './crestfall-studio/client/dist/'),
+    outDir: './client/dist/',
   },
 });
