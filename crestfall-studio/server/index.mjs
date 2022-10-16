2978896a452172271eab3fb60a613666d8b89298
joshxyzhimself <joshxyzhimself@gmail.com>
2022-10-17 05:02:56 +0800

    save

--- crestfall-studio/server/index.mjs
+++ crestfall-studio/server/index.mjs
@@ -1,5 +1,96 @@
 // @ts-check
 
-import fs from 'fs';
+/**
+ * @typedef {import('./index').user} user
+ */
 
-console.log('Hello world!');
+import assert from 'assert';
+import crypto from 'crypto';
+import readline from 'node:readline/promises';
+import * as luxon from 'luxon';
+import * as uwu from 'modules/uwu.mjs';
+import * as hs256 from 'modules/hs256.mjs';
+import env from '../../crestfall-core/env.mjs';
+
+const secret = env.get('PGRST_JWT_SECRET');
+
+/**
+ * What's in here:
+ * - Internal authentication client, in your command line interface
+ * - Enter the commands in your console to test them!
+ * - /ct - creates an anon token
+ * - /su - creates an anon token, signs-up a new user
+ */
+process.nextTick(async () => {
+  const rli = readline.createInterface({ input: process.stdin, output: process.stdout });
+  const readline_loop = async () => {
+    try {
+      const line = await rli.question('');
+      const segments = line.split(' ');
+      const command = segments[0];
+      switch (command) {
+        default: {
+          console.log(`Unhandled: ${line}`);
+          break;
+        }
+      }
+    } catch (e) {
+      console.error(e);
+    }
+    process.nextTick(readline_loop);
+  };
+  process.nextTick(readline_loop);
+});
+
+/**
+ * What's in here:
+ * - Internal authentication server
+ */
+process.nextTick(async () => {
+
+  const app = uwu.uws.App({});
+  app.options('/*', uwu.use_middleware(async (response, request) => {
+    response.status = 204;
+    const access_control_request_method = request.headers.get('access-control-request-method');
+    const origin = request.headers.get('origin');
+    const access_control_allow_headers = ['content-type'];
+    if (request.headers.has('access-control-request-headers') === true) {
+      const access_control_request_headers = request.headers.get('access-control-request-headers').split(',');
+      access_control_allow_headers.push(...access_control_request_headers);
+    }
+    response.headers.set('Access-Control-Allow-Origin', origin);
+    response.headers.set('Access-Control-Allow-Methods', access_control_request_method);
+    response.headers.set('Access-Control-Allow-Headers', access_control_allow_headers.join(','));
+    response.headers.set('Access-Control-Max-Age', '300');
+  }));
+
+  app.post('/test', uwu.use_middleware(async (response, request) => {
+    response.headers.set('Access-Control-Allow-Origin', request.headers.get('origin'));
+    response.json = { data: null, error: null };
+    try {
+      response.status = 200;
+      response.json.data = {};
+    } catch (e) {
+      console.error(e);
+      const error = { request, name: e.name, code: e.code, message: e.message, stack: e.stack };
+      response.status = 500;
+      response.json.error = error;
+    }
+  }));
+
+  /**
+   * @type {import('modules/uwu').middleware}
+   */
+  const serve_404 = async (response, request) => {
+    const error = { request, name: 'Not Found', code: 'ERR_NOT_FOUND', message: null, stack: null };
+    response.status = 404;
+    response.json = { data: null, error };
+  };
+  app.get('/*', uwu.use_middleware(serve_404));
+  app.post('/*', uwu.use_middleware(serve_404));
+  app.patch('/*', uwu.use_middleware(serve_404));
+  app.put('/*', uwu.use_middleware(serve_404));
+  app.del('/*', uwu.use_middleware(serve_404));
+
+  await uwu.serve_http(app, uwu.port_access_types.EXCLUSIVE, 9090);
+});
