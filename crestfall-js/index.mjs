2e02bd285b81fb95635d7c63f271369434af132b
joshxyzhimself <joshxyzhimself@gmail.com>
2022-10-22 22:41:52 +0800

    save

---
+++ crestfall-js/index.mjs
@@ -0,0 +1,48 @@
+// @ts-check
+
+import { assert } from 'modules/assert.mjs';
+import fetch from 'cross-fetch';
+
+export const CRESTFALL_AUTH_PORT = 9090;
+
+/**
+ * @param {string} protocol
+ * @param {string} host
+ * @param {string} default_token
+ */
+export const initialize = (protocol, host, default_token) => {
+  assert(typeof protocol === 'string');
+  assert(typeof host === 'string');
+  assert(typeof default_token === 'string');
+
+  /**
+   * @type {string}
+   */
+  const authenticated_token = null;
+
+  /**
+   * @param {string} email
+   * @param {string} password
+   */
+  const sign_in = async (email, password) => {
+    assert(typeof email === 'string', 'ERR_INVALID_EMAIL');
+    assert(typeof password === 'string', 'ERR_INVALID_PASSWORD');
+    const request_method = 'POST';
+    const request_url = `${protocol}://${host}:${CRESTFALL_AUTH_PORT}/sign-up`;
+    const request_token = authenticated_token || default_token;
+    const request_headers = {
+      'Content-Type': 'application/json; charset=utf-8',
+      'Authorization': `Bearer ${request_token}`,
+    };
+    const request_body = JSON.stringify({ email: email, password: password });
+    const response = await fetch(request_url, { method: request_method, headers: request_headers, body: request_body });
+    assert(response.headers.has('content-type') === true);
+    assert(response.headers.get('content-type').includes('application/json') === true);
+    const status = response.status;
+    const body = await response.json();
+    return { status, body };
+  };
+
+  const client = { sign_in };
+  return client;
+};
---
+++ crestfall-js/index.test.mjs
@@ -0,0 +1,45 @@
+// @ts-check
+
+import assert from 'assert';
+import * as crestfall from './index.mjs';
+import env from './env.mjs';
+import * as hs256 from 'modules/hs256.mjs';
+import * as luxon from 'luxon';
+
+assert(env.has('PGRST_JWT_SECRET') === true);
+assert(env.has('PGRST_JWT_SECRET_IS_BASE64') === true);
+assert(env.get('PGRST_JWT_SECRET_IS_BASE64') === 'true');
+
+const PGRST_JWT_SECRET = env.get('PGRST_JWT_SECRET');
+console.log({ PGRST_JWT_SECRET });
+
+/**
+ * Create non-expiring anon token.
+ * @param {string} secret
+ * @returns {string}
+ */
+const create_anon_token = (secret) => {
+  const header = { alg: 'HS256', typ: 'JWT' };
+  const payload = {
+    iat: luxon.DateTime.now().toSeconds(),
+    nbf: luxon.DateTime.now().toSeconds(),
+    exp: null,
+    iss: 'crestfall',
+    aud: 'crestfall',
+    sub: null,
+    role: 'anon',
+    email: null,
+  };
+  const token = hs256.create_token(header, payload, secret);
+  return token;
+};
+
+const anon_token = create_anon_token(PGRST_JWT_SECRET);
+console.log({ anon_token });
+
+const client = crestfall.initialize('http', 'localhost', anon_token);
+
+process.nextTick(async () => {
+  const sign_in_response = await client.sign_in('alice@gmail.com', 'test');
+  console.log(JSON.stringify({ sign_in_response }, null, 2));
+});
