03e89bd3ca1f5d039917d29e14af76466628ed64
joshxyzhimself <joshxyzhimself@gmail.com>
2022-10-30 04:04:03 +0800

    save

--- scripts/crestfall.sh
+++ scripts/crestfall.sh
@@ -4,14 +4,14 @@
 # https://news.ycombinator.com/item?id=26825300
 # https://linuxize.com/post/bash-if-else-statement/
 # https://github.com/torokmark/assert.sh/blob/main/assert.sh
-# bash ./crestfall.sh --alias
-# bash ./crestfall.sh --install 93e7e98db4a7a9d240b490dfc9d143477297affcbc24bea0de964523f01b58ad https://gist.githubusercontent.com/joshxyzhimself/5e249ac0f4027b57aa71622f2f86a1b0/raw/1accf4558e575c80a4de6c06631c2c1c7d3d3f7b/example.sh
+# crestfall.sh --install OK
+# crestfall.sh 93e7e98db4a7a9d240b490dfc9d143477297affcbc24bea0de964523f01b58ad https://gist.githubusercontent.com/joshxyzhimself/5e249ac0f4027b57aa71622f2f86a1b0/raw/1accf4558e575c80a4de6c06631c2c1c7d3d3f7b/example.sh
 
 eq() {
   if [ $1 -eq $2 ]; then
     return 0
   else
-    printf "$3";
+    printf "$3"
     exit 1
   fi
 }
@@ -20,7 +20,7 @@ ge() {
   if [ $1 -ge $2 ]; then
     return 0
   else
-    printf "$3";
+    printf "$3"
     exit 1
   fi
 }
