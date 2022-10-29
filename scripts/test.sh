3dafef1e83bb74756746078aff01e78de9e63e8c
joshxyzhimself <joshxyzhimself@gmail.com>
2022-10-29 18:11:24 +0800

    test

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,8 +1,9 @@
 # References
-# https://gist.githubusercontent.com/joshxyzhimself/5e249ac0f4027b57aa71622f2f86a1b0/raw/1accf4558e575c80a4de6c06631c2c1c7d3d3f7b/example.sh
 # https://news.ycombinator.com/item?id=26825300
+# https://linuxize.com/post/bash-if-else-statement/
+# https://github.com/torokmark/assert.sh/blob/main/assert.sh
 
-# bash ./test.sh 6f0a181195f86a726dba153e3877e2a0c53bfdd015640be0896c4f6b91be7007 https://gist.githubusercontent.com/joshxyzhimself/5e249ac0f4027b57aa71622f2f86a1b0/raw/1accf4558e575c80a4de6c06631c2c1c7d3d3f7b/example.sh
+# bash ./test.sh --alias
 # bash ./test.sh --install 93e7e98db4a7a9d240b490dfc9d143477297affcbc24bea0de964523f01b58ad https://gist.githubusercontent.com/joshxyzhimself/5e249ac0f4027b57aa71622f2f86a1b0/raw/1accf4558e575c80a4de6c06631c2c1c7d3d3f7b/example.sh
 
 eq() {
