e48bd4098b1da0911e4a8b0ecaf77fea68a94d6c
joshxyzhimself <joshxyzhimself@gmail.com>
2022-10-30 09:20:35 +0800

    save

--- scripts/crestfall.sh
+++ scripts/crestfall.sh
@@ -7,6 +7,8 @@
 # crestfall.sh --install OK
 # crestfall.sh 93e7e98db4a7a9d240b490dfc9d143477297affcbc24bea0de964523f01b58ad https://gist.githubusercontent.com/joshxyzhimself/5e249ac0f4027b57aa71622f2f86a1b0/raw/1accf4558e575c80a4de6c06631c2c1c7d3d3f7b/example.sh
 
+# https://www.shellcheck.net/
+
 eq() {
   if [ $1 -eq $2 ]; then
     return 0
