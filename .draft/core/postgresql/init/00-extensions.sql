88f3de05208e74c68b1a6a46a19a2343280dfefc
joshxyzhimself <joshxyzhimself@gmail.com>
2022-12-25 01:23:45 +0800

    save

--- .draft/core/postgresql/init/00-extensions.sql
+++ .draft/core/postgresql/init/00-extensions.sql
@@ -1,9 +1,10 @@
 
 CREATE SCHEMA IF NOT EXISTS "extensions";
-CREATE EXTENSION IF NOT EXISTS "pgcrypto" WITH SCHEMA "extensions";
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp" WITH SCHEMA "extensions";
-CREATE EXTENSION IF NOT EXISTS "http" WITH SCHEMA "extensions";
-CREATE EXTENSION IF NOT EXISTS "pgaudit" WITH SCHEMA "extensions";
+
+-- CREATE EXTENSION IF NOT EXISTS "pgcrypto" WITH SCHEMA "extensions";
+-- CREATE EXTENSION IF NOT EXISTS "http" WITH SCHEMA "extensions";
+-- CREATE EXTENSION IF NOT EXISTS "pgaudit" WITH SCHEMA "extensions";
 -- SELECT (unnest(headers)).* FROM extensions.http_get('https://example.com/');
 
 -- CREATE EXTENSION IF NOT EXISTS "pg_cron" WITH SCHEMA "extensions";
--- postgresql/entrypoint/defaults.sql
+++ postgresql/entrypoint/defaults.sql
@@ -1,3 +1,4 @@
+-- extensions
 CREATE SCHEMA IF NOT EXISTS "extensions";
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp" WITH SCHEMA "extensions";
 
