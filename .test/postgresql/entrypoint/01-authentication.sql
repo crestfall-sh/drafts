80c8ee9441492efb997536e6dae59b356fb671a3
joshxyzhimself <joshxyzhimself@gmail.com>
2023-01-14 16:49:38 +0800

    save

--- .test/postgresql/entrypoint/01-authentication.sql
+++ .test/postgresql/entrypoint/01-authentication.sql
@@ -1,87 +1,17 @@
--- initialize extensions
-CREATE SCHEMA IF NOT EXISTS "extensions";
-CREATE EXTENSION IF NOT EXISTS "unaccent" WITH SCHEMA "extensions";
-CREATE EXTENSION IF NOT EXISTS "pgcrypto" WITH SCHEMA "extensions";
-CREATE EXTENSION IF NOT EXISTS "uuid-ossp" WITH SCHEMA "extensions";
-CREATE EXTENSION IF NOT EXISTS "http" WITH SCHEMA "extensions";
-CREATE EXTENSION IF NOT EXISTS "pgaudit" WITH SCHEMA "extensions";
-CREATE EXTENSION IF NOT EXISTS "pgjwt" WITH SCHEMA "extensions";
-CREATE EXTENSION IF NOT EXISTS "pgsodium";
-
--- test unaccent:
--- todo:
--- [x] lower-case
--- [x] normalize NFKC
--- [x] unaccent / case-fold
--- [x] regex validate for username / email
-SELECT
-    lower('Maße'),
-    lower('Hôtel'),
-	lower(normalize(extensions.unaccent('Maße'), NFKC)),
-	lower(normalize(extensions.unaccent('Hôtel'), NFKC)),
-    lower(normalize(extensions.unaccent('Maße@gmail.com'), NFKC)) ~ '^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+[.][A-Za-z]+$' as "Maße@gmail.com",
-    lower(normalize(extensions.unaccent('Hôtel@gmail.com'), NFKC)) ~ '^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+[.][A-Za-z]+$' as "Hôtel@gmail.com",
-    lower(normalize(extensions.unaccent('example @gmail.com'), NFKC)) ~ '^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+[.][A-Za-z]+$' as "example @gmail.com",
-    lower(normalize(extensions.unaccent('example@gmail.com'), NFKC)) ~ '^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+[.][A-Za-z]+$' as "example@gmail.com";
-    
-
--- test uuid-ossp:
-SELECT extensions.uuid_generate_v4();
-
--- test pgjwt:
-SELECT extensions.sign('{"sub":"1234567890","name":"John Doe","admin":true}', 'secret', 'HS256');
-
--- test pgsodium:
-SELECT * FROM pgsodium.crypto_box_new_keypair();
-
--- test pgsodium:
-SELECT
-	"password"::text,
-    "salt"::text,
-    pgsodium.crypto_pwhash("password", "salt")::text as "hash"
-	FROM (
-      SELECT
-      	'password'::bytea as "password",
-      	pgsodium.crypto_pwhash_saltgen() as "salt"
-    )as table_one;
-
--- test pgsodium, expected hash: \x970d0e80120556642c641d67fea013ba3b8d249d041e92e1550220a6061dc457
-SELECT
-	"password"::text,
-    "salt"::text,
-    pgsodium.crypto_pwhash("password", "salt")::text as "hash"
-	FROM (
-      SELECT
-      	'password'::bytea as "password",
-      	'\x15708e9a39491dda0f3fd181cf09d886'::bytea as "salt"
-    )as table_one;
-
--- test http:
-SELECT (unnest(headers)).* FROM extensions.http_get('https://example.com/');
-
--- create authenticator role for postgrest
+-- "authenticator" role for postgrest
 CREATE ROLE authenticator LOGIN NOINHERIT;
 
--- create anon role for postgrest
+-- "anon" role for postgrest
 CREATE ROLE anon NOLOGIN NOINHERIT;
 ALTER ROLE anon SET statement_timeout = '5s';
 GRANT USAGE ON SCHEMA public TO anon; -- schema-level permissions
 GRANT SELECT ON ALL TABLES IN SCHEMA public TO anon; -- grant for table-level, rls for row-level permissions
 GRANT anon TO authenticator;
 
--- create public.tasks table, http://0.0.0.0:5433/tasks
-CREATE TABLE public.tasks (
-	"id" uuid DEFAULT extensions.uuid_generate_v4() PRIMARY KEY,
-	"description" text NOT NULL
-);
-INSERT INTO public.tasks ("description") VALUES
-	('Paint a self-portrait.'),
-	('Build a house.');
-
--- Authentication: create private schema
+-- "private" schema
 CREATE SCHEMA private;
 
--- Authentication: create private.users table
+-- "private.users" table
 CREATE TABLE private.users (
     "id" uuid DEFAULT extensions.uuid_generate_v4() PRIMARY KEY,
     "email" text NOT NULL,
@@ -100,14 +30,14 @@ CREATE TABLE private.users (
 );
 ALTER TABLE private.users ENABLE ROW LEVEL SECURITY;
 
--- Authentication: create public.users table
+-- "public.users"
 CREATE TABLE public.users (
   "id" uuid DEFAULT extensions.uuid_generate_v4() PRIMARY KEY,
   "email" text NOT NULL
 );
 ALTER TABLE public.users ENABLE ROW LEVEL SECURITY;
 
--- Authentication: create private.sub function
+-- "private.sub"
 CREATE OR REPLACE FUNCTION private.sub()
 RETURNS uuid
 LANGUAGE SQL STABLE
@@ -118,7 +48,7 @@ AS $$
 	)::uuid
 $$;
 
--- Authentication: create private.role function
+-- "private.role"
 CREATE OR REPLACE FUNCTION private.role()
 RETURNS text
 LANGUAGE SQL STABLE
@@ -129,7 +59,7 @@ AS $$
 	)::text
 $$;
 
--- Authentication: create private.email function
+-- "private.email"
 CREATE OR REPLACE FUNCTION private.email()
 RETURNS text
 LANGUAGE SQL STABLE
@@ -140,29 +70,7 @@ AS $$
 	)::text
 $$;
 
--- Automatic Schema Cache Reloading: event function
--- https://postgrest.org/en/stable/schema_cache.html#schema-reloading
-CREATE OR REPLACE FUNCTION public.pgrst_watch()
-    RETURNS event_trigger
-    LANGUAGE plpgsql
-    AS $$
-BEGIN
-    NOTIFY pgrst, 'reload schema';
-END;
-$$;
-
--- Automatic Schema Cache Reloading: trigger function every ddl_command_end event
--- https://postgrest.org/en/stable/schema_cache.html#schema-reloading
-CREATE EVENT TRIGGER pgrst_watch ON ddl_command_end EXECUTE PROCEDURE public.pgrst_watch();
-
--- http://localhost:5433/rpc/add_them?a=1&b=2
--- https://postgrest.org/en/stable/api.html#stored-procedures
-CREATE FUNCTION add_them(a integer, b integer)
-    RETURNS integer AS $$
-    SELECT a + b;
-$$ LANGUAGE SQL IMMUTABLE;
-
--- Authentication: Sign-up
+-- "sign_up" function
 CREATE FUNCTION sign_up(email_parameter text, password_parameter text) RETURNS void AS $$
 DECLARE
     user_record record;
@@ -183,7 +91,7 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql;
 
--- Authentication: Sign-in
+-- "sign_in" function
 CREATE FUNCTION sign_in(email_parameter text, password_parameter text) RETURNS void AS $$
 DECLARE
     user_record record;
@@ -206,6 +114,7 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql;
 
+-- test "sign_up", "sign_in"
 SELECT COUNT(*) FROM private.users;
 SELECT sign_up('admin@local.host', 'test1234');
 SELECT sign_up('alice@local.host', 'test1234');
