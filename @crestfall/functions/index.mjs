328cb8c81dd6b665c7ddc18db0f1400ccf9fc462
joshxyzhimself <joshxyzhimself@gmail.com>
2023-03-02 18:55:43 +0800

    add get_tarball

--- @crestfall/functions/index.mjs
+++ @crestfall/functions/index.mjs
@@ -34,6 +34,69 @@ console.log(`action: ${action}`);
  *
  */
 
+/**
+ * @param {string} owner
+ * @param {string} repository
+ */
+const get_commits = async (owner, repository) => {
+  assert(typeof owner === 'string');
+  assert(typeof repository === 'string');
+  const url = new URL('https://api.github.com/');
+  url.pathname = `/repos/${owner}/${repository}/commits`;
+  url.searchParams.set('type', 'private');
+  const response = await fetch(url.toString(), {
+    headers: { Authorization: `Bearer ${token}` },
+  });
+  assert(response.status === 200);
+  assert(response.headers.has('content-type') === true);
+  assert(response.headers.get('content-type').includes('application/json') === true);
+  /**
+   * @type {import('./index').commit[]}
+   */
+  // @ts-ignore
+  const commits = await response.json();
+  assert(commits instanceof Array);
+  return commits;
+};
+
+/**
+ * @param {string} owner
+ * @param {string} repository
+ * @param {string} reference
+ * @description downloads and extracts the repository tarball.
+ */
+const get_tarball = async (owner, repository, reference) => {
+  assert(typeof owner === 'string');
+  assert(typeof repository === 'string');
+  assert(typeof reference === 'string');
+  const reference_short = reference.substring(0, 7);
+  const url = new URL('https://api.github.com/');
+  url.pathname = `/repos/${owner}/${repository}/tarball/${reference}`;
+  const response = await fetch(url.toString(), {
+    headers: { Authorization: `Bearer ${token}` },
+  });
+  assert(response.status === 200);
+  assert(response.headers.has('content-type') === true);
+  assert(response.headers.get('content-type').includes('application/x-gzip') === true);
+  const data = await response.arrayBuffer();
+  const data_buffer = Buffer.from(data);
+  const snapshot_id = `${owner}-${repository}-${reference_short}`;
+  const snapshot_tarball_path = path.join(process.cwd(), `${snapshot_id}.tar.gz`);
+  fs.writeFileSync(snapshot_tarball_path, data_buffer);
+  child_process.spawnSync('tar', ['--extract', '--ungzip', `--file=${snapshot_tarball_path}`]);
+  const snapshot_directory_path = path.join(process.cwd(), snapshot_id);
+  fs.unlinkSync(snapshot_tarball_path);
+  const snapshot = {
+    owner,
+    repository,
+    reference,
+    reference_short,
+    snapshot_id,
+    snapshot_directory_path,
+  };
+  return snapshot;
+};
+
 process.nextTick(async () => {
   switch (action) {
     case 'lr': {
@@ -59,61 +122,15 @@ process.nextTick(async () => {
       });
       break;
     }
-    case 'lc': {
-      console.log('listing commits..');
-      const owner = 'joshxyzhimself';
-      const repository = 'example-functions';
-      const url = new URL('https://api.github.com/');
-      url.pathname = `/repos/${owner}/${repository}/commits`;
-      url.searchParams.set('type', 'private');
-      const response = await fetch(url.toString(), {
-        headers: { Authorization: `Bearer ${token}` },
-      });
-      console.log(`status: ${response.status}`);
-      assert(response.status === 200);
-      console.log(`headers: ${response.headers}`);
-      assert(response.headers.has('content-type') === true);
-      assert(response.headers.get('content-type').includes('application/json') === true);
-      /**
-       * @type {import('./index').commit[]}
-       */
-      // @ts-ignore
-      const commits = await response.json();
-      assert(commits instanceof Array);
-      console.log(JSON.stringify({ commits }, null, 2));
-      break;
-    }
     case 'tar': {
-      console.log('fetching tarball..');
       const owner = 'joshxyzhimself';
       const repository = 'example-functions';
-      const reference = 'e115704494f8855eae4eb856eb1f96aff0d4187c';
-      console.log({ reference });
-      const reference_short = reference.substring(0, 7);
-      console.log({ reference_short });
-      const url = new URL('https://api.github.com/');
-      url.pathname = `/repos/${owner}/${repository}/tarball/${reference}`;
-      const response = await fetch(url.toString(), {
-        headers: { Authorization: `Bearer ${token}` },
-      });
-      console.log(`status: ${response.status}`);
-      assert(response.status === 200);
-      console.log(`headers: ${response.headers}`);
-      assert(response.headers.has('content-type') === true);
-      assert(response.headers.get('content-type').includes('application/x-gzip') === true);
-      console.log(response.headers.get('content-type'));
-      const data = await response.arrayBuffer();
-      const data_buffer = Buffer.from(data);
-      console.log({ data_buffer });
-      const snapshot_id = `${owner}-${repository}-${reference_short}`;
-      console.log({ snapshot_id });
-      const snapshot_tarball_path = path.join(process.cwd(), `${snapshot_id}.tar.gz`);
-      console.log({ snapshot_tarball_path });
-      fs.writeFileSync(snapshot_tarball_path, data_buffer);
-      child_process.spawnSync('tar', ['--extract', '--ungzip', `--file=${snapshot_tarball_path}`]);
-      const snapshot_extract_path = path.join(process.cwd(), snapshot_id);
-      console.log(snapshot_extract_path, fs.existsSync(snapshot_extract_path));
-      fs.unlinkSync(snapshot_tarball_path);
+      const commits = await get_commits(owner, repository);
+      assert(commits instanceof Array);
+      const commit = commits[0];
+      assert(commit instanceof Object);
+      const snapshot = await get_tarball(owner, repository, commit.sha);
+      console.log(snapshot);
       break;
     }
     default: {
