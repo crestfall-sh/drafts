acf90cb3f086c67717702567b76eaa74f19dcbc9
joshxyzhimself <joshxyzhimself@gmail.com>
2023-03-02 09:47:09 +0800

    save

--- @crestfall/functions/index.mjs
+++ @crestfall/functions/index.mjs
@@ -1,3 +1,6 @@
 // @ts-check
 
+// make it work on cli first, before creating a gui for it.
+// curl as the user interface should work for most cases, same for caddy ui
+
 import fetch from 'node-fetch';
